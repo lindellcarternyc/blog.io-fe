8961bf2961f433ca1b69a1f7b5af18f93a6462f4
Lindell Carter <lindellcarternyc@gmail.com>
2021-08-15 16:47:03 -0400

    creates global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;1,600&display=swap");
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    width: 100%;
+    position: relative;
+    font-family: 'poppins', sans-serif;
+  }
+`
+
+export default GlobalStyles
