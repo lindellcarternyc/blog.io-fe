f71d651ea31f6498b8b487440a4e951e4de84420
Lindell Carter <lindellcarternyc@gmail.com>
2021-08-17 13:42:58 -0400

    adds top margin to layout component

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,20 @@
+import styled from 'styled-components'
 import Navbar from './Navbar/Navbar'
 
 interface LayoutProps {
   children?: JSX.Element | JSX.Element[]
 }
 
+const ContentWrapper = styled.div`
+  margin-top: 60px;
+`
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <div>
       <Navbar />
-      {children}
+      <ContentWrapper>
+        {children}
+      </ContentWrapper>
     </div>
   )
 }
